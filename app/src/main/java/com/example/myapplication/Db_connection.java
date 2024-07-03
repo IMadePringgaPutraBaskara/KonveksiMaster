0687c4913bc59fe6e3944b4848216807140648c1
NandaFebian <nandafebiandananjaya@gmail.com>
2024-07-04 02:50:31 +0800

    Memperbarui tampilan selamat datang, sekarang berisi nama user sesuai sesi login, menambahkan button logout

--- app/src/main/java/com/example/myapplication/Db_connection.java
+++ app/src/main/java/com/example/myapplication/Db_connection.java
@@ -1,7 +1,7 @@
 package com.example.myapplication;
 
 public class Db_connection {
-    public static  String ip = "192.168.2.103";
+    public static  String ip = "192.168.1.91";
 
     public static final String urlLogin = "http://"+ip+"/KonveksiMaster/api_login.php";
     public static final String urlRegister = "http://"+ip+"/KonveksiMaster/api_register.php";
