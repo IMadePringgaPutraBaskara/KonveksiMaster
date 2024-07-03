e4f8a6c437cc8b63cc1be35db247cdf4d8245150
dbask <dbaskaraxd@gmail.com>
2024-07-03 21:03:42 +0800

    Big Update Home act Tshirt app dll

--- app/src/main/java/com/example/myapplication/TshirtSale.kt
+++ app/src/main/java/com/example/myapplication/TshirtSale.kt
@@ -1,4 +1,29 @@
 package com.example.myapplication
 
-class TshirtSale {
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class TshirtSale : AppCompatActivity() {
+
+    private lateinit var value:TextView
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tshirt_sale)
+
+        value = findViewById(R.id.value)
+    }
+
+    fun increasement(v: View){
+        count++
+        value.text = count .toString()
+    }
+
+    fun decreasement(v: View){
+        if (count > 0) count--
+        value.text = count.toString()
+    }
 }
