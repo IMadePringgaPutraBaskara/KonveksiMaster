dcc61b695b1bb7b0e38fc130a6987f03c45aa083
dbask <dbaskaraxd@gmail.com>
2024-07-03 17:44:56 +0800

    Big Update Home act Tshirt app dll

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_app)
+
+        val imageShirt = findViewById<ImageView>(R.id.imageShirt)
+        val imageTShirt = findViewById<ImageView>(R.id.imageTShirt)
+        val imagePolo = findViewById<ImageView>(R.id.imagePolo)
+        val imageHoodie = findViewById<ImageView>(R.id.imageHoodie)
+
+        imageShirt.setOnClickListener {
+            // Tambahkan aksi saat imageShirt diklik
+            val intent = Intent(this@HomeActivity, ShirtSale::class.java)
+            startActivity(intent)
+        }
+
+        imageTShirt.setOnClickListener {
+            // Tambahkan aksi saat imageTShirt diklik
+            val intent = Intent(this@HomeActivity, TshirtSale::class.java)
+            startActivity(intent)
+        }
+
+        imagePolo.setOnClickListener {
+            // Tambahkan aksi saat imagePolo diklik
+            val intent = Intent(this@HomeActivity, PoloActivity::class.java)
+            startActivity(intent)
+        }
+
+        imageHoodie.setOnClickListener {
+            // Tambahkan aksi saat imageHoodie diklik
+            val intent = Intent(this@HomeActivity, HoodieActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
